ecc53c58e6055aa9c613b0abc77a1d943126ccee
Cameron Leathers <99282208+CameonTL@users.noreply.github.com>
2022-04-06 14:24:16 -0500

    Took out some keys
    Removed unnecessary unique keys and simplified foreign and primary keys the tables

--- Scheduler.sql
+++ Scheduler.sql
@@ -10,7 +10,7 @@ CREATE DATABASE Scheduler;
  uemail		varchar(40) not null,
  upassword	varchar(25) not null,
  CONSTRAINT pk_userLogin primary key (uemail),
- CONSTRAINT uk_userLogin unique (uemail)
+ CONSTRAINT uk_userlogin unique (uname)
  );
  
  DROP TABLE IF EXISTS USER_INFO;
@@ -21,7 +21,7 @@ CREATE DATABASE Scheduler;
  uposition	varchar(15),
  ucompany	varchar(30) not null,
  CONSTRAINT pk_userInfo primary key (uemail),
- CONSTRAINT uk_userInfo unique (uemail),
+ CONSTRAINT uk_userInfo unique (uname),
  CONSTRAINT fk_userInfo foreign key (uemail) references USER_LOGIN(uemail)
  );
  
@@ -30,8 +30,7 @@ CREATE DATABASE Scheduler;
  cname		varchar(30) not null,
  cnumber	int(11) not null,
  clocation	varchar(40) not null,
- CONSTRAINT pk_companyInfo primary key (cname),
- CONSTRAINT uk_compnayInfo unique (cname)
+ CONSTRAINT pk_companyInfo primary key (cname)
  );
  
  DROP TABLE IF EXISTS AVAILABLE;
@@ -47,7 +46,6 @@ CREATE DATABASE Scheduler;
  thuClose	time,
  friOpen	time,
  friClose	time,
- CONSTRAINT pk_availabiltiy primary key (uemail),
  CONSTRAINT fk_availabiltiy foreign key (uemail) references USER_LOGIN(uemail)
  );
  
